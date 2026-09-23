e516707476c4675400c0397d19a032ad5854cb64
nithin raj <nithinraj9100@gmail.com>
2026-09-23 10:06:28 +0530

    fix(core): repair migration registry merge marker after rebase

--- packages/core/src/database/migration.gen.ts
+++ packages/core/src/database/migration.gen.ts
@@ -46,7 +46,7 @@ import m43 from "./migration/20260812213948_worktree.js"
 import m44 from "./migration/20260819222447_session_viewed_state.js"
 import m45 from "./migration/20260823191254_nullable_workspace_binding.js"
 import m46 from "./migration/20260910120000_clear_v1_session_permission.js"
-<import m47 from "./migration/20260912115926_session-goal.js"
+import m47 from "./migration/20260912115926_session-goal.js"
 import m48 from "./migration/20260923013825_project_time_active.js"
 
 export const migrations = [
